a36f9ad8bc9d5bea1a5d803619e8b918bbbaf636
Jeroin <cjeroin@gmail.com>
2020-10-15 16:46:20 +0800

    whileLoop

---
+++ whileLoop/src/whileLoop/whileLoop.java
@@ -0,0 +1,14 @@
+package whileLoop;
+
+public class whileLoop {
+
+	public static void main(String[] args) {
+		int age = 0;//initialization
+		while(age < 19) {//condition
+			System.out.println("Minor age " + age);
+			age++;//update
+		}
+
+	}
+
+}
